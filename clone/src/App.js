720dd5f33c9b3c0fe1738581bcc2b8078f5424b8
Prudence Maphutha <prumaphutha@gmail.com>
2025-02-19 15:07:39 +0200

    Normalize line endings

---
+++ clone/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sidebar"></div>
+      <div className="main"></div>
+    </div>
+  );
+}
+
+export default App;
